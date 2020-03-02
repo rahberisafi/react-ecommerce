8da7ac0097e2c8b3d999ea6418d4379a6d007e5e
iamisafi <isafi@gmail.com>
2020-03-02 23:03:32 +0600

    start project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'; 
+import './App.css'; 
+import PageDisplay from './components/PageDisplay'; 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const App = () => { 
+  // const [profile, setPro] = useState(null);
+  // const [userId, setUID] = useState(null);
+  // const [cart, setCart] = useState([]);
+  
+  return ( 
+   <PageDisplay /> 
+   ); 
+}; 
+
+export default App;
---
+++ src/components/PageDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PageDisplay = () => {
+    return (
+        <h1>hello</h1> 
+    )
+}
+
+export default PageDisplay;
